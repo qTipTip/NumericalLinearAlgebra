d35a50e5eb3d927306fcd5e409ab7b5e472c551b
Ivar Stangeby <istangeby@gmail.com>
2017-08-23 14:39:17 +0200

    Added comments. Tested that lu factorization works for large n, and for a small test case.

--- NumericalLinearAlgebra/matrices/tests/test_tdma.py
+++ NumericalLinearAlgebra/matrices/tests/test_tdma.py
@@ -6,7 +6,7 @@ from NumericalLinearAlgebra.matrices.matrices import tdma_diagonals, tdma_lu
 
 class TestTdma_diagonals(TestCase):
     def test_tdma_diagonals(self):
-
+        """ Given a matrix A, check whether diagonals are fetched properly"""
         A = np.array([
             [1, 2, 3],
             [2, 3, 4],
@@ -24,7 +24,7 @@ class TestTdma_diagonals(TestCase):
         np.testing.assert_almost_equal(sup, expected_sup)
 
     def test_tdma_diagonals_typeerror(self):
-
+        """ Check that the function tdma_diagonals raises a TypeError if not an ndarray"""
         A = [
             [1, 2, 3],
             [2, 3, 4],
@@ -34,7 +34,7 @@ class TestTdma_diagonals(TestCase):
         self.assertRaises(TypeError, tdma_diagonals, A)
 
     def test_tdma_lu(self):
-
+        """ Checks that factorization is computed correctly, small known solution"""
         A = np.array([
             [4, 1, 0],
             [1, 4, 1],
@@ -50,11 +50,18 @@ class TestTdma_diagonals(TestCase):
         np.testing.assert_almost_equal(expected_l, computed_l)
         np.testing.assert_almost_equal(expected_u, computed_u)
 
-    def test_tdma_lu_assemble(self):
-        n = 10000
+    def test_tdma_lu_mult(self):
+        """ Checks that the factorization is computed properly and that multiplying L, U yields A."""
+        n = 100
 
-        a = np.full(1, n-1)
-        c = np.full(1, n-1)
-        d = np.full(4, n)
+        a = np.full(n-1, 1, dtype=np.float64)
+        c = np.full(n-1, 1, dtype=np.float64)
+        d = np.full(n, 4, dtype=np.float64)
 
-        A = np.diag(d) + np.diag
+        A = np.diag(d, 0) + np.diag(a, -1) + np.diag(c, 1)
+        l, u = tdma_lu(a, d, c)
+
+        L = np.eye(n) + np.diag(l, -1)
+        U = np.diag(u) + np.diag(c, 1)
+
+        np.testing.assert_almost_equal(A, L.dot(U))
