1cf64a52c8ae8f5cc2354e69882f7eb5ac64f676
Ivar Stangeby <istangeby@gmail.com>
2017-08-30 13:40:13 +0200

    Updated setup.py

--- NLA/__init__.py
+++ NLA/__init__.py
@@ -1,3 +1,3 @@
-from NLA.matrices import tdma_lu, tdma_solve, tdma_diagonals
+from NLA.matrices import tdma_lu, tdma_solve, tdma_diagonals, cbackwardsolve, rbackwardsolve, rforwardsolve, cforwardsolve
 from NLA.splines import spline_interpolation, spline_evaluation, find_subintervals
 from NLA.profiling import timer
